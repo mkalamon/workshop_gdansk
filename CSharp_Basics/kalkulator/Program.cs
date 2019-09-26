d3fa435dbfd96a494b65444a962d44c5ccd76c4a
mkalamon <marta.kalamon@gmail.com>
2019-09-26 10:09:40 +0200

    komit z komentarzami czyli źle

--- CSharp_Basics/kalkulator/Program.cs
+++ CSharp_Basics/kalkulator/Program.cs
@@ -20,13 +20,97 @@ namespace kalkulator
             string number2 = Console.ReadLine();
             int numberTwo = int.Parse(number2);
 
-            int add = numberOne + numberTwo;
+            Console.WriteLine("Podaj działanie");
+            string operation = Console.ReadLine();
 
-            Console.WriteLine($"Wynik: {add}");
+            double result = 0;
+
+            switch(operation)
+            {
+                case "+":
+                    result = Add(numberOne, numberTwo);
+                    break;
+
+                case "-":
+                    result = Substract(numberOne, numberTwo);
+                    break;
+
+                case "*":
+                    result = Multiple(numberOne, numberTwo);
+                    break;
+
+                case "/":
+                    result = Divide(numberOne, numberTwo);
+                    break;
+
+                default:
+                    Console.WriteLine("Nieznany symbol");
+                    break;
+
+            }
+            Console.WriteLine(result);
+
+            //if (operation == "+")
+            //{
+            //    result = Add(numberOne, numberTwo);
+            //}
+
+            //else if (operation == "-")
+            //{
+            //    result = Substract(numberOne, numberTwo);
+            //}       
+
+            //else if (operation == "*")
+            //{
+            //    result = Multiple(numberOne, numberTwo);
+            //}
+
+            //else if (operation == "/")
+            //{
+            //    result = Divide(numberOne, numberTwo);
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Podano zły znak");
+            //}
+
+
+
+            int add = Add(numberOne, numberTwo);
+            int substract = Substract(numberOne, numberTwo);
+            int multiple = Multiple(numberOne, numberTwo);
+            double divide = Divide(numberOne, numberTwo);
+
+            Console.WriteLine($"Wynik dodawania: {add}");
+            Console.WriteLine($"Wynik odejmowania: {substract}");
+            Console.WriteLine($"Wynik mnożenia: {multiple}");
+            Console.WriteLine($"Wynik dzielenia: {divide}");
 
 
             //Console.ReadLine();
             Console.ReadKey();
+        }
+
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber + secondNumber;
+            return result;
+        }
+        public static int Substract(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber - secondNumber;
+            return result;
+        }
+        public static int Multiple(int firstNumber, int secondNumber)
+        {
+            int result = firstNumber * secondNumber;
+            return result;
+        }
+        public static double Divide(double firstNumber, double secondNumber)
+        {
+            double result = firstNumber / secondNumber;
+            return result;
         }
     }
 }
